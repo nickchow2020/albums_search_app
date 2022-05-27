e7e4a854b5da79773faa9e4d2f04592f23c2977f
nick <smz1680@gmail.com>
2022-05-27 11:43:54 -0700

    finsih body header layout

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log("yes")
+let searchResult = []
+
+
+
+
+async function getJSON(name){
+  let  URL = `https://itunes.apple.com/search?term=${name}&media=music&entity=album&attribute=artistTerm&limit=200`
+  try{
+      const albumsList  = await fetch(URL);
+      return albumsList.json();
+  }catch(err){
+      throw err
+  }
+}
+
+
+
+getJSON("gaga")
+.then(data => {
+  searchResult = data
+  console.log(searchResult)
+})
+.catch(err => console.log("Error :",err))
+
+
+
