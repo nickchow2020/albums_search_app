1684f184eb05d947fa3a6545816d4020f0df3c62
nick <smz1680@gmail.com>
2022-05-27 12:26:19 -0700

    able to fetch data into display

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+const cardDisplayArea = document.querySelector("[data-cards-display-area]")
+
+
+
 let searchResult = []
-
-
-
 
 async function getJSON(name){
   let  URL = `https://itunes.apple.com/search?term=${name}&media=music&entity=album&attribute=artistTerm&limit=200`
@@ -14,11 +15,42 @@ async function getJSON(name){
 }
 
 
+function render() {
+  const allCard = searchResult.results.map( obj => cardTemplate(obj)).join("")
+
+  cardDisplayArea.innerHTML = allCard
+}
+
+
+
+
+function cardTemplate(data){
+  let template = 
+  `
+  <div class="card" id=${data.artistId}>
+    <div class="card__cover">
+      <img src="${data.artworkUrl100}" alt="image">
+    </div>
+
+    <div class="card__album">
+      <p>
+        ${data.collectionCensoredName}
+      </p>
+    </div>
+  </div>
+  
+  `
+
+  return template
+}
+
+
 
 getJSON("gaga")
 .then(data => {
   searchResult = data
   console.log(searchResult)
+  render()
 })
 .catch(err => console.log("Error :",err))
 
