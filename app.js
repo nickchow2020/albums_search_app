8663036421e4e2b7afa368fa2f133203ac2de63d
nick <smz1680@gmail.com>
2022-06-02 09:12:07 -0700

    fix icon layout and fix cors issues

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ let searchResult = []
 async function getJSON(name){
   let  URL = `https://itunes.apple.com/search?term=${name}&media=music&entity=album&attribute=artistTerm&limit=200`
   try{
-      const albumsList  = await fetch(URL);
+      const albumsList  = await fetchJsonp(URL);
       return albumsList.json();
   }catch(err){
       throw err
